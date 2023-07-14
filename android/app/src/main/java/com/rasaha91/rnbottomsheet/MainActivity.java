cac9eccc6f4f7d5f8f39a45cc9e814018a06f5ef
Ranesh Saha <rasaha@microsoft.com>
2023-07-14 15:30:27 -0700

    Working RN hosted in bottom sheet

--- android/app/src/main/java/com/rasaha91/rnbottomsheet/MainActivity.java
+++ android/app/src/main/java/com/rasaha91/rnbottomsheet/MainActivity.java
@@ -2,16 +2,45 @@ package com.rasaha91.rnbottomsheet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Build;
+import android.provider.Settings;
 import android.widget.Button;
 
+import com.facebook.react.PackageList;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.ReactRootView;
+import com.facebook.react.common.LifecycleState;
+import com.facebook.soloader.SoLoader;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
+    private final int OVERLAY_PERMISSION_REQ_CODE = 1;
+    private ReactRootView mReactRootView;
+    private ReactInstanceManager mReactInstanceManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SoLoader.init(this, false);
+        mReactRootView = new ReactRootView(this);
+        List<ReactPackage> packages = new PackageList(getApplication()).getPackages();
+
+        mReactInstanceManager = ReactInstanceManager.builder()
+                .setApplication(getApplication())
+                .setCurrentActivity(this)
+                .setBundleAssetName("index.android.bundle")
+                .setJSMainModulePath("index")
+                .addPackages(packages)
+                .setUseDeveloperSupport(BuildConfig.DEBUG)
+                .setInitialLifecycleState(LifecycleState.RESUMED)
+                .build();
+
         setContentView(R.layout.activity_main);
         Button mButton = findViewById(R.id.button);
         mButton.setOnClickListener(v -> showBottomSheetDialog());
@@ -19,7 +48,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void showBottomSheetDialog() {
         final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
-        bottomSheetDialog.setContentView(R.layout.bottom_sheet_dialog_layout);
+        mReactRootView.startReactApplication(mReactInstanceManager, "MyReactNativeApp", null);
+        bottomSheetDialog.setContentView(mReactRootView);
         bottomSheetDialog.show();
     }
 }
