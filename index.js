7bc8e965a301f43ab312767583e747b671dcf3e3
Ranesh Saha <rasaha@microsoft.com>
2023-07-14 16:44:53 -0700

    simplify view tree

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ import {AppRegistry, StyleSheet, Text, View} from 'react-native';
 
 const HelloWorld = () => {
   return (
-    <View>
       <Text style={styles.hello}>Hello, World</Text>
-    </View>
   );
 };
 var styles = StyleSheet.create({
