51cf95b5a4307a2c6243a7583201256e3c87fdaa
Ranesh Saha <rasaha@microsoft.com>
2023-07-14 16:33:07 -0700

    Working version in 0.68

--- index.js
+++ index.js
@@ -3,16 +3,12 @@ import {AppRegistry, StyleSheet, Text, View} from 'react-native';
 
 const HelloWorld = () => {
   return (
-    <View style={styles.container}>
+    <View>
       <Text style={styles.hello}>Hello, World</Text>
     </View>
   );
 };
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-  },
   hello: {
     fontSize: 20,
     textAlign: 'center',
