99355eb4f374f317258048c4b1d923ffdf71af48
Ranesh Saha <rasaha@microsoft.com>
2023-07-14 13:48:27 -0700

    Add basic android app that shows bottom sheet dialog

---
+++ android/app/src/main/java/com/rasaha91/rnbottomsheet/MainActivity.java
@@ -0,0 +1,25 @@
+package com.rasaha91.rnbottomsheet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button mButton = findViewById(R.id.button);
+        mButton.setOnClickListener(v -> showBottomSheetDialog());
+    }
+
+    private void showBottomSheetDialog() {
+        final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
+        bottomSheetDialog.setContentView(R.layout.bottom_sheet_dialog_layout);
+        bottomSheetDialog.show();
+    }
+}
